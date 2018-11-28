4c94cf3a33df8bfcf78e8f25c609e33a41ee5482
RodriguesMy <43988541+RodriguesMy@users.noreply.github.com>
2018-11-28 16:37:13 +0200

    myria

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 const app = 'team6';
+const HTTP='https://codecyprus.org/th/api/'+'start?player=';
 
+//START------------------------------------------------------------
 function triggerStart() {
     let player = document.getElementById('player').value;
     let uuid = getParameter('uuid');
@@ -7,7 +9,7 @@ function triggerStart() {
 }
 
 function apiStart(player, app, uuid) {
-    let url = 'https://codecyprus.org/th/api/start?player=' + player + '&app=' + app + '&treasure-hunt-id=' + uuid;
+    let url = HTTP + player + '&app=' + app + '&treasure-hunt-id=' + uuid;
     let xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.onload = handleStart;
     xmlHttpRequest.open('GET', url, true);
@@ -22,7 +24,7 @@ function handleStart() {
     } else {
         let errorDiv = document.getElementById('errors');
         let errorMessage = o['errorMessages'][0];
-        errorDiv.innerHTML = '<b>' + errorMessage + '</b>';
+        errorDiv.innerHTML = alert(errorMessage);
     }
 }
 
@@ -30,3 +32,4 @@ function getParameter(name) {
     let url = new URL(window.location.href);
     return url.searchParams.get(name);
 }
+//-------------------------------------------------------------
