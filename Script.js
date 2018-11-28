cc0941c1456581330ee4f85878aa0a348c755ac5
RodriguesMy <43988541+RodriguesMy@users.noreply.github.com>
2018-11-28 15:17:55 +0200

    ml

--- Script.js
+++ Script.js
@@ -1,5 +1,32 @@
-function setName(){
-    document.getElementById("name");
-    console.log(document.getElementById("name"));
+const app = 'team6';
+
+function triggerStart() {
+    let player = document.getElementById('player').value;
+    let uuid = getParameter('uuid');
+    apiStart(player, app, uuid);
 }
 
+function apiStart(player, app, uuid) {
+    let url = 'https://codecyprus.org/th/api/start?player=' + player + '&app=' + app + '&treasure-hunt-id=' + uuid;
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onload = handleStart;
+    xmlHttpRequest.open('GET', url, true);
+    xmlHttpRequest.send();
+}
+
+function handleStart() {
+    console.log('start response: ' + this.responseText);
+    let o = JSON.parse(this.responseText);
+    if(o['status'] === 'OK') {
+        // todo
+    } else {
+        let errorDiv = document.getElementById('errors');
+        let errorMessage = o['errorMessages'][0];
+        errorDiv.innerHTML = '<b>' + errorMessage + '</b>';
+    }
+}
+
+function getParameter(name) {
+    let url = new URL(window.location.href);
+    return url.searchParams.get(name);
+}
