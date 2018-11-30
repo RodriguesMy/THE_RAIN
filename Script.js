c70a5c86364a8d4c0269224383fa387f57cdf19e
RodriguesMy <43988541+RodriguesMy@users.noreply.github.com>
2018-11-30 23:02:47 +0200

    Treasure Hunt

--- Script.js
+++ Script.js
@@ -1,31 +1,33 @@
 const app = 'team6';
 const API_PREFIX = 'https://codecyprus.org/th/api/';
-//LIST-------------------------------------------------------------
-function showList() {
+
+//LIST---------------------------------------
+
+function showList(){
     apiList();
 }
 
-function apiList() {
-    let url = API_PREFIX + 'list';
-    let xmlHttpRequest = new XMLHttpRequest();
-    xmlHttpRequest.onload = handleList;
-    xmlHttpRequest.open('GET', url, true);
+function apiList(){
+    let url=API_PREFIX+'list';
+    let xmlHttpRequest=new XMLHttpRequest();
+    xmlHttpRequest.onload=handleList;
+    xmlHttpRequest.open('GET',url,true);
     xmlHttpRequest.send();
 }
 
-function handleList() {
+function handleList(){
     console.log(this.responseText);
-    let o = JSON.parse(this.responseText);
-    let thsArray = o['treasureHunts'];
-    let html = '<ul>';
-    for(let i in thsArray) {
-        let th = thsArray[i];
+    let o=JSON.parse(this.responseText);
+    let thsArray=o['treasureHunts'];
+    let html='<ul>';
+    for(let i in thsArray){
+        let th=thsArray[i];
         console.log(th);
-        html += '<li><a href="sampleTreasureHunt.html?uuid=' + th['uuid'] + '">' + th['name'] + '</a>' + '</li>';
+        html+='<li><a href="Start.html?uuid=' + th['uuid'] + '">' + th['name'] + '</a>' + '</li>';
     }
-    html += '</ul>';
-    let thListDiv = document.getElementById('th-list');
-    thListDiv.innerHTML = '<h1>Treasure Hunts:</h1>' + html;
+    html+='</ul>';
+    let thsListDiv=document.getElementById('th-list');
+    thsListDiv.innerHTML='<h1>Treasure Hunts:</h1>'+html;
 }
 
 function getParameter(name) {
@@ -34,20 +36,15 @@ function getParameter(name) {
 }
 
 
+//LOCATION--------------------------------------------------------
+function apiLocation(session, lat, lng) {
+    let url = API_PREFIX + 'location?session=' + session + '&latitude=' + lat + '&longitude=' + lng;
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open('GET', url, true);
+    xmlHttpRequest.send();
+}
+apiLocation();
 
-// //LOCATION--------------------------------------------------------
-// function apiLocation(session, lat, lng) {
-//     let url = API_PREFIX + 'location?session=' + session + '&latitude=' + lat + '&longitude=' + lng;
-//     let xmlHttpRequest = new XMLHttpRequest();
-//     xmlHttpRequest.open('GET', url, true);
-//     xmlHttpRequest.send();
-// }
-// apiLocation();
-//
-//
-//
-//
-//
 //START------------------------------------------------------------
 function triggerStart() {
     let player = document.getElementById('player').value;
@@ -55,7 +52,6 @@ function triggerStart() {
     apiStart(player,app,uuid);
 }
 
-//https://codecyprus.org/th/api/start?player=Homer&app=simpsons-app&treasure-hunt-id=ag9nfmNvZGVjeXBydXNvcmdyGQsSDFRyZWFzdXJlSHVudBiAgICAvKGCCgw
 function apiStart(player,app,uuid) {
     let url = API_PREFIX +'start?player='+player+'&app='+app+'&treasure-hunt-id='+uuid;
     let xmlHttpRequest = new XMLHttpRequest();
@@ -63,93 +59,114 @@ function apiStart(player,app,uuid) {
     xmlHttpRequest.open('GET', url, true);
     xmlHttpRequest.send();
 }
-
-var listDiv = document.getElementById("divList");
+let thisDiv=document.getElementById['errorsDiv'];
 function handleStart() {
-console.log("START");
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log(this.responseText);
-            var o = JSON.parse(this.responseText);
-            for (let i = 0; i < o.treasureHunts.length; i++) {
-                var inList = document.createElement("li");
-                var listItem = document.createElement("a");
-                listItem.innerHTML = o.treasureHunts[i].name;
-                listItem.href = "https://codecyprus.org/th/api/start?player=Homer&app=simpsons&treasure-hunt-id="+o.treasureHunts[i].uuid;
-                inList.appendChild(listItem);
-                listDiv.appendChild(inList);
-            }
-        }
-        else
-            {
-              //  let errorDiv = document.getElementById('errors');
-              //  let errorMessage = o['errorMessages'][0];
-              // errorDiv.innerHTML = alert(errorMessage);
-            }
-        };
-
-    xhttp.open("GET", "https://codecyprus.org/th/api/list", true);
-    xhttp.send();
+    console.log(this.responseText);
+    let o = JSON.parse(this.responseText);
+    if (o['status'] === 'OK') {
+        window.location.href = 'Question.html?session=' + o['session'];
+    } else {
+        console.log('errorMessages: ' + o['errorMessages'][0]);
+        thisDiv.innerHTML = o['errorMessages'][0];
+    }
 }
+
+
 
 function getParameter(name) {
     let url = new URL(window.location.href);
     return url.searchParams.get(name);
 }
-// //-------------------------------------------------------------
-// function triggerQuestion(){
-//     let session=document.getElementById('session'.value);
-//     questionStart(session);
+//QUESTION-------------------------------------------------
+function triggerQuestion(){
+    let session=getParameter('session');
+    questionStart(session);
+}
+function questionStart(session){
+    let url=API_PREFIX+'question?session='+session;
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onload=handleQuestion;
+    xmlHttpRequest.open('GET',url,true);
+    xmlHttpRequest.send();
+}
+
+function handleQuestion(){
+    console.log(this.responseText);
+    let o=JSON.parse(this.responseText);
+    //if(o[status]==='OK'){
+        document.getElementById('th-question').innerHTML='Question:</br>'+o['questionText'];
+   // }else{
+       // let thisDiv=document.getElementById['error'];
+       // thisDiv.innerHTML = o['errorMessages'][0];
+   // }
+}
+
+
+//ANSWER--------------------------------------------------
+function triggerAnswer(){
+    let answer=document.getElementById('answer').value;
+    let session=getParameter('session');
+    answerStart(session,answer);
+}
+
+function answerStart(session,answer){
+    let url=API_PREFIX+'answer?session='+session+'&answer='+answer;
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onload=handleAnswer;
+    xmlHttpRequest.open('GET',url,true);
+    xmlHttpRequest.send();
+}
+
+function handleAnswer(){
+    let o=JSON.parse(this.responseText);
+   //if(o[status]==='OK'){
+        document.getElementById('result').innerHTML='Result: '+o['correct'];
+        if(o['numOfQuestions']===0){
+            triggerQuestion();
+        }else{
+            triggerScore();
+        }
+    // }else{
+    //     let errorFix=document.getElementById('errors');
+    //     let errorMessage = o['errorMessages'][0];
+    //     errorDiv.innerHTML = alert(errorMessage);
+    // }
+}
+
+function triggerScore(){
+    let session=getParameter('session');
+}
+//COOKIES----------------------------------------------------
+// function setCookie(session) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+//     var expires = "expires="+d.toUTCString();
+//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 // }
 //
-// function questionStart(session){
-//     let url=API_PREFIX+player+'&app='+app+'&treasure-hunt-id='+uuid+'&session='+session;
-//     let xmlHttpRequest = new XMLHttpRequest();
-//     xmlHttpRequest.onload=handleQuestion;
-//     xmlHttpRequest.open('GET',url,true);
-//     xmlHttpRequest.send();
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var ca = document.cookie.split(';');
+//     for(var i = 0; i < ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
 // }
 //
-//    thListDiv.innerHTML = '<h1>Treasure Hunts</h1>' + html;
-// function handleQuestion(){
-//     console.log(this.responseText);
-//     let o=JSON.parse(this.responseText);
-//     if(o['completed']===true){
-//
+// function checkCookie() {
+//     var user = getCookie("username");
+//     if (user != "") {
+//         alert("Welcome again " + user);
+//     } else {
+//         user = prompt("Please enter your name:", "");
+//         if (user != "" && user != null) {
+//             setCookie("username", user, 365);
+//         }
 //     }
 // }
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
-    }
-}
